780e037af5ed45cca9ad303d25e08a69eec481ce
Sylvan <100524681+xiaownwang@users.noreply.github.com>
2024-12-01 18:15:24 -0500

    Add files via upload

---
+++ fine_tuning_test.sh
@@ -0,0 +1,9 @@
+python -u fine_tuning_test.py \
+--plms --outdir results \
+--config configs/v1.yaml \
+--ckpt checkpoints/model.ckpt \
+--image_folder dataset/testdata/image \
+--mask_folder dataset/testdata/mask \
+--reference_folder dataset/testdata/reference \
+--seed 321 \
+--scale 5
